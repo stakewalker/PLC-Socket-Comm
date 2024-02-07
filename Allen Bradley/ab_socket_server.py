6f5a771e3f17e7ac5a9b6ad4986a520f2b233ac3
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-02-07 13:40:42 -0300

    Create ab_socket_server.py
    Script developed to communicate with Allen Bradley PLCs via TCP sockets

---
+++ Allen Bradley/ab_socket_server.py
@@ -0,0 +1,33 @@
+import socket
+import struct
+#from rich import print  # Recommended for better visualization
+
+def main():
+    # PLC and PC addresses
+    plc_address = ('192.168.1.36', 2021)
+    pc_address = ('192.168.1.123', 2020)
+    # Create server socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(pc_address)
+    server_socket.listen(1)
+    print(f'Listening on {pc_address[0]} port {pc_address[1]}')
+
+    while True:
+        # Wait for a connection
+        print('Waiting for connection...')
+        connection, client_address = server_socket.accept()
+        try:
+            print(f'Connection established with {client_address}')
+            # Send SINT[4] array
+            data_to_send = bytes([18, 222, 33, 44])
+            connection.sendall(data_to_send)
+            print('Array sent (SINT[4]):', [i for i in data_to_send])
+            # Receive data from PLC
+            received_data = [i for i in connection.recv(16)]
+            print('Data received from PLC:', list(received_data))
+        finally:
+            # Close the connection
+            connection.close()
+
+if __name__ == "__main__":
+    main()
